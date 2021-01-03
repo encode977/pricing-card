0520122e97713212bf869d08fe3a96b5a008cbd4
Lokindra Dangi <pachar.com@gmail.com>
2021-01-03 08:43:08 +0545

    pro card complete

--- src/Pro.js
+++ src/Pro.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./Pro.css";
+import Option from "./Option";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 
 function Pro() {
   return (
     <div className="pro">
-      <h3>Pro</h3>
+      <div className="pro__title">
+        <h3>Pro</h3>
+        <h4>For agencies</h4>
+      </div>
+      <h1>$40</h1>
+      <div className="pro__option">
+        <Option Icon={CheckCircleIcon} title="3 users" />
+        <Option Icon={CheckCircleIcon} title="Unlimited downloads" />
+        <Option Icon={CheckCircleIcon} title="Fully editable files" />
+        <Option Icon={CheckCircleIcon} title="Customs packs" />
+      </div>
+      <button className="pro__btn">Subscribe</button>
     </div>
   );
 }
